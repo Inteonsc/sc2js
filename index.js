d230982e77db61bfcba2ad5012fad1efda96fc91
Lili <7150288+Inteonsc@users.noreply.github.com.>
2026-03-23 00:31:43 +0000

    updated package

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-export function test(){
-    return "Hello World!";
-}
+export { SC2Replay } from './src/api/SC2Replay.js';
+export * from './src/api/ReplayUtils.js';
+export { BitPackedBuffer, BitPackedDecoder, VersionedDecoder } from './src/reader/decoder.js';
+export { loadProtocol } from './src/reader/protocolLoader.js';
+export * from './src/reader/protocolDecoder.js';
+export * from '.src/reader/mpq.js'
+export { default as Attributes } from './src/reader/attributes.js';
