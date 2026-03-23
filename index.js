27fda7de0461a0adb707f0d417b9dd0a33bc8280
Lili <7150288+Inteonsc@users.noreply.github.com.>
2026-03-23 00:35:25 +0000

    should actually be fixed

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ export * from './src/api/ReplayUtils.js';
 export { BitPackedBuffer, BitPackedDecoder, VersionedDecoder } from './src/reader/decoder.js';
 export { loadProtocol } from './src/reader/protocolLoader.js';
 export * from './src/reader/protocolDecoder.js';
-export * from '.src/reader/mpq.js'
+export * from './src/reader/mpq.js'
 export { default as Attributes } from './src/reader/attributes.js';
